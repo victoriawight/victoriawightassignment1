6dcefb8056fdab41fd377e174ce7df3fd35c8ec7
Wight <victoria.wight@uleth.ca>
2016-03-01 23:49:24 -0700

    final

---
+++ final/script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+	// alert("welcome to alberta endangered animals. For gitHub link, go to Contact Us");
+$( "#img" ).hover(function() {
+  $( ".img-responsive" ).fadeTo( "slow", 0.5, function() {
+    // Animation complete.
+  });
+});
+}
